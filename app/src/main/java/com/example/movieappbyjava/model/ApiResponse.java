7862c7caf2ec6428ca11570bcf961b34a1336cfe
Phat <22130206@st.hcmuaf.edu.vn>
2025-05-16 15:21:48 +0700

    add movie to home

---
+++ app/src/main/java/com/example/movieappbyjava/model/ApiResponse.java
@@ -0,0 +1,11 @@
+package com.example.movieappbyjava.model;
+
+import java.util.List;
+
+public class ApiResponse {
+    private List<Movie> items;
+
+    public List<Movie> getItems() {
+        return items;
+    }
+}
---
+++ app/src/main/java/com/example/movieappbyjava/network/KKPhimApi.java
@@ -0,0 +1,27 @@
+package com.example.movieappbyjava.network;
+
+import com.example.movieappbyjava.model.ApiResponsePhim;
+import com.example.movieappbyjava.model.ApiResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface KKPhimApi {
+    @GET("danh-sach/phim-moi-cap-nhat")
+    Call<ApiResponse> getLatestMovies(@Query("page") int page);
+
+    @GET("v1/api/danh-sach/phim-le")
+    Call<ApiResponsePhim> getSingleMovies();
+
+    @GET("v1/api/danh-sach/phim-bo")
+    Call<ApiResponsePhim> getSeriasMovies();
+
+    @GET("v1/api/danh-sach/hoat-hinh")
+    Call<ApiResponsePhim> getAnimeMovies();
+
+
+    @GET("v1/api/danh-sach/tv-shows")
+    Call<ApiResponsePhim> getTvShowMovies();
+
+}
