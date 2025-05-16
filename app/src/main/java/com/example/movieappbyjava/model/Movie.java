a6b3df0d41379109f090f4f523feec4cbba1edd7
Phat <22130206@st.hcmuaf.edu.vn>
2025-05-16 15:21:48 +0700

    add movie to home

---
+++ app/src/main/java/com/example/movieappbyjava/model/Movie.java
@@ -0,0 +1,11 @@
+package com.example.movieappbyjava.model;
+
+public class Movie {
+    private String name;
+    private String slug;
+    private String poster_url;
+
+    public String getName() { return name; }
+    public String getSlug() { return slug; }
+    public String getPoster_url() { return poster_url; }
+}
