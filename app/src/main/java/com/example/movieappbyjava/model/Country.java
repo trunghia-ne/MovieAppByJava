3c9dde9a6d501053b19074b002a7c1d9ee650b93
Phat <22130206@st.hcmuaf.edu.vn>
2025-05-17 23:08:04 +0700

    add ui detail film & watch film

---
+++ app/src/main/java/com/example/movieappbyjava/model/Country.java
@@ -0,0 +1,11 @@
+package com.example.movieappbyjava.model;
+
+public class Country {
+    private String id;
+    private String name;
+    private String slug;
+
+    public String getId() { return id; }
+    public String getName() { return name; }
+    public String getSlug() { return slug; }
+}
