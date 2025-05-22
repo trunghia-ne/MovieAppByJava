d64da52ea879b791589f16a10f6740d0da46dc72
Phat <22130206@st.hcmuaf.edu.vn>
2025-05-22 23:28:28 +0700

    connect project to Firebase

--- app/src/main/java/com/example/movieappbyjava/LoginActivity.java
+++ app/src/main/java/com/example/movieappbyjava/LoginActivity.java
@@ -2,15 +2,19 @@ package com.example.movieappbyjava;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.FirebaseApp;
 
 public class LoginActivity extends AppCompatActivity {
     TextView registerNow;
