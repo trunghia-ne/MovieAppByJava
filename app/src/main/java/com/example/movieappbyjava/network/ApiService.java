deac370f36a43e2b0badf246472d1947f26ad755
DaoDuyNhat29 <22130194@st.hcmuaf.edu.vn>
2025-05-29 15:59:50 +0700

    vnpay

--- app/src/main/java/com/example/movieappbyjava/network/ApiService.java
+++ app/src/main/java/com/example/movieappbyjava/network/ApiService.java
@@ -1,9 +1,18 @@
 package com.example.movieappbyjava.network;
 
+import com.example.movieappbyjava.model.PaymentUrlResponse;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 public interface ApiService {
     @GET("hello")
     Call<String> getHello();
+
+    @POST("/api/pay")
+    Call<PaymentUrlResponse> createPayment(@Query("amount") int amount, @Query("userId") String userId);
+
+
 }
